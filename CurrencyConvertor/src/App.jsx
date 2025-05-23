e013e5e19a9af1bbc9aac3b825a62360e2e8ff7f
sarthak-vyas183 <vyassarthak183@gmail.com>
2025-05-23 16:13:38 +0530

    refactor App component to implement currency conversion functionality

--- CurrencyConvertor/src/App.jsx
+++ CurrencyConvertor/src/App.jsx
@@ -4,30 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState();
+  const [currency, setCurrency] = useState("INR")
+
+  const currencyConvertor = async () => {
+     const response = await fetch(`https://v6.exchangerate-api.com/v6/d81a7bf6bfb96b2b4ff02bea/latest/${currency}`)
+     const res = await response.json();
+     setData(JSON.stringify(res.conversion_rates));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <label htmlFor="input-number">Enter Currency</label> <br />
+    <input className='text-3xl' type="text"/>
+    <p>{data}</p>
     </>
   )
 }
